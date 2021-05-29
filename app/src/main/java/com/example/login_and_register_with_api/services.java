0872fafc441255b841f03429ad8aee33c82a04d5
yousef <54207974+7hj4@users.noreply.github.com>
2021-05-30 00:28:18 +0300

    Update services.java

--- app/src/main/java/com/example/login_and_register_with_api/services.java
+++ app/src/main/java/com/example/login_and_register_with_api/services.java
@@ -1,13 +1,13 @@
 package com.example.login_and_register_with_api;
 
 public class services {
+    
+    // here input your server
+    public static String url_login = "http://yourserver/api_login/login/index.php";
 
-    public static String url_login = "http://192.168.43.154/api_login/login/index.php";
+    public static String url_register = "http://yourserver/api_login/signup/index.php";
 
-    public static String url_register = "http://192.168.43.154/api_login/signup/index.php";
-
-    //public static String url_profile = "http://192.168.43.154/api_mobile/User/profile/";
-
+   
 
 
 }
